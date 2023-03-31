ec6e18757a5e007883f342b96331851178ca394a
SH-indeyong <118088909+SH-indeyong@users.noreply.github.com>
2023-04-01 04:28:58 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.29 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -4,13 +4,10 @@ function solution(left, right) {
     
     for(var i=left; i<=right; i++){
         for (var j=1; j<=i; j++){
-            if(i%j == 0)
-                measure++;
+            if(i%j == 0) measure++;
         }
-        if(measure%2 == 0)
-            result += i;
-        else
-            result -= i;
+        if(measure%2 == 0) result += i;
+        else result -= i;
         measure=0;
     }
     return result;

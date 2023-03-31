1e0b8a611bd0f171b3420f9133dc8105cadb2fd7
SH-indeyong <118088909+SH-indeyong@users.noreply.github.com>
2023-04-01 04:49:35 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,6 +1,6 @@
 function solution(s) {
     for(let i=0; i<s.length; i++){
-        if(isNaN(s.split("")[i]) === true) return false;
+        if(isNaN(s.split("")[i])) return false;
     }
     return s.length === 4 || s.length === 6 ? true : false;
 }

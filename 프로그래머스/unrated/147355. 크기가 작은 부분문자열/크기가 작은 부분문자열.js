80ebcb471113e8d0edc5e98c899e8faece657cc1
SH-indeyong <118088909+SH-indeyong@users.noreply.github.com>
2023-07-04 19:22:09 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,11 +1,11 @@
 function solution(t, p) {
     var answer = 0;
     var num = 0;
+    var length = p.length;
     // p의 길이
     // t를 p의 길이만큼 잘라서 비교 answer++
     
-    var length = p.toString().length;
-    for (var i=0; i<t.length - length + 1; i++){
+    for (var i=0; i<=t.length - length; i++){
         num = t.substr(i, length);
         if(num <= p)
             answer++;

712646b0d0308984a9110ed566f390aa4ddf0c48
SH-indeyong <118088909+SH-indeyong@users.noreply.github.com>
2024-09-27 17:49:26 +0900

    [level 1] Title: 2016년, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,4 +1,13 @@
 function solution(a, b) {
+    const month = [31,29,31,30,31,30,31,31,30,31,30,31];
+    const weekDay = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"];
+
+    let day = b
+    for(let i = 0 ; i < a-1 ; i++)
+        day += month[i];
+
+    return weekDay[day % 7];
+    
     // Date의 month는 인덱스로 되어있다.
-    return new Date(2016, a - 1, b).toString().slice(0, 3).toUpperCase();
+    // return new Date(2016, a - 1, b).toString().slice(0, 3).toUpperCase();
 }
--- 프로그래머스/1/12901. 2016년/README.md
+++ 프로그래머스/1/12901. 2016년/README.md
@@ -1,10 +1,10 @@
 # [level 1] 2016년 - 12901 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901#) 
 
 ### 성능 요약
 
-메모리: 34.4 MB, 시간: 0.37 ms
+메모리: 33.5 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 27일 17:46:11
+2024년 09월 27일 17:49:24
 
 ### 문제 설명
 
